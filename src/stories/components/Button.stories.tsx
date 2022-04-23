865314d21847a884af244b2bf279be69b032f748
William Suaza <wills777@hotmail.com>
2022-04-23 17:33:56 -0500

    feat: the button component and the corresponding stories are created

---
+++ src/stories/components/Button.stories.tsx
@@ -0,0 +1,57 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import '../wirvi-styles.scss';
+import { Icon } from '../icon';
+import Button from '../../components/button/Button';
+
+export default {
+  title: 'Wirvi-roka/Button',
+  component: Button
+} as ComponentMeta<typeof Button>
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Basic = Template.bind({});
+Basic.args = {
+  text: 'Hello Word!',
+}
+
+export const TextChildren: ComponentStory<typeof Button> = (args) => (
+  <Button {...args}>Label Children</Button>
+);
+
+export const WithIcon = Template.bind({});
+WithIcon.args = {
+  icon: <Icon />,
+  text: 'Hello Word!'
+}
+
+export const OnlyIcon = Template.bind({});
+OnlyIcon.args = {
+  icon: <Icon />
+}
+
+export const FullScreen = Template.bind({});
+FullScreen.args = {
+  isFullwidth: true,
+  text: 'Full Screen'
+}
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  isDisabled: true,
+  text: 'Disabled'
+}
+
+export const WithChildren: ComponentStory<typeof Button> = (args) => (
+  <Button {...args}>
+    <span style={{ marginRight: '10px' }}>Label Children</span>
+    <Icon />
+  </Button>
+);
+
+export const WithLoading = Template.bind({});
+WithLoading.args = {
+  isLoading: true,
+  text: 'Hello Word!'
+}
